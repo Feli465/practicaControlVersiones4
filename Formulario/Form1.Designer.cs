7f98d299f46c0ec06730732f9e03bf9dffcb991f
felicidad.garcia@iesmarenostrum.com <felicidad.garcia@iesmarenostrum.com>
2020-05-22 16:04:29 +0200

    cambios pendientes

--- Formulario/Form1.Designer.cs
+++ Formulario/Form1.Designer.cs
@@ -37,87 +37,107 @@
             this.tTelefono = new System.Windows.Forms.TextBox();
             this.tListaSocios = new System.Windows.Forms.TextBox();
             this.bGuardar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 47);
+            this.label1.Location = new System.Drawing.Point(86, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(87, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(333, 47);
+            this.label2.Location = new System.Drawing.Point(666, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(100, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Apellidos";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(602, 47);
+            this.label3.Location = new System.Drawing.Point(1204, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.Size = new System.Drawing.Size(50, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "TLF";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(53, 182);
+            this.label4.Location = new System.Drawing.Point(106, 350);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.Size = new System.Drawing.Size(129, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Lista Socios";
             // 
             // tNombre
             // 
-            this.tNombre.Location = new System.Drawing.Point(110, 39);
+            this.tNombre.Location = new System.Drawing.Point(220, 75);
+            this.tNombre.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tNombre.Name = "tNombre";
-            this.tNombre.Size = new System.Drawing.Size(159, 20);
+            this.tNombre.Size = new System.Drawing.Size(314, 31);
             this.tNombre.TabIndex = 4;
             // 
             // tApellidos
             // 
-            this.tApellidos.Location = new System.Drawing.Point(402, 39);
+            this.tApellidos.Location = new System.Drawing.Point(804, 75);
+            this.tApellidos.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tApellidos.Name = "tApellidos";
-            this.tApellidos.Size = new System.Drawing.Size(159, 20);
+            this.tApellidos.Size = new System.Drawing.Size(314, 31);
             this.tApellidos.TabIndex = 5;
             // 
             // tTelefono
             // 
-            this.tTelefono.Location = new System.Drawing.Point(634, 44);
+            this.tTelefono.Location = new System.Drawing.Point(1268, 85);
+            this.tTelefono.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tTelefono.Name = "tTelefono";
-            this.tTelefono.Size = new System.Drawing.Size(159, 20);
+            this.tTelefono.Size = new System.Drawing.Size(314, 31);
             this.tTelefono.TabIndex = 6;
             // 
             // tListaSocios
             // 
-            this.tListaSocios.Location = new System.Drawing.Point(56, 210);
+            this.tListaSocios.Location = new System.Drawing.Point(112, 404);
+            this.tListaSocios.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tListaSocios.Multiline = true;
             this.tListaSocios.Name = "tListaSocios";
-            this.tListaSocios.Size = new System.Drawing.Size(707, 164);
+            this.tListaSocios.Size = new System.Drawing.Size(1410, 312);
             this.tListaSocios.TabIndex = 7;
             // 
             // bGuardar
             // 
-            this.bGuardar.Location = new System.Drawing.Point(618, 105);
+            this.bGuardar.Location = new System.Drawing.Point(1236, 202);
+            this.bGuardar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.bGuardar.Name = "bGuardar";
-            this.bGuardar.Size = new System.Drawing.Size(144, 63);
+            this.bGuardar.Size = new System.Drawing.Size(288, 121);
             this.bGuardar.TabIndex = 8;
             this.bGuardar.Text = "Guardar";
             this.bGuardar.UseVisualStyleBackColor = true;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(276, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Modificación Feli 22052020";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.bGuardar);
             this.Controls.Add(this.tListaSocios);
             this.Controls.Add(this.tTelefono);
@@ -127,6 +147,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -145,6 +166,7 @@
         private System.Windows.Forms.TextBox tTelefono;
         private System.Windows.Forms.TextBox tListaSocios;
         private System.Windows.Forms.Button bGuardar;
+        private System.Windows.Forms.Label label5;
     }
 }
 
